d2d84a7df6a4daf544728eb6ad87abba9a2376c4
henrymbuguak <henrymbuguak@gmail.com>
2019-08-25 14:37:07 +0300

    mpesa credentials file

---
+++ mpesa_api/mpesa_credentials.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+from datetime import datetime
+import base64
+
+
+class MpesaC2bCredential:
+    consumer_key = 'cHnkwYIgBbrxlgBoneczmIJFXVm0oHky'
+    consumer_secret = '2nHEyWSD4VjpNh2g'
+    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+
+
+class MpesaAccessToken:
+    r = requests.get(MpesaC2bCredential.api_URL,
+                     auth=HTTPBasicAuth(MpesaC2bCredential.consumer_key, MpesaC2bCredential.consumer_secret))
+    mpesa_access_token = json.loads(r.text)
+    validated_mpesa_access_token = mpesa_access_token['access_token']
+
+
+class LipanaMpesaPpassword:
+    lipa_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    Business_short_code = "174379"
+    passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+
+    data_to_encode = Business_short_code + passkey + lipa_time
+
+    online_password = base64.b64encode(data_to_encode.encode())
+    decode_password = online_password.decode('utf-8')
+
--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 import requests
 from requests.auth import HTTPBasicAuth
+from datetime import datetime
+import base64
 import json
 
 
@@ -14,3 +16,28 @@ def getAccessToken(request):
     validated_mpesa_access_token = mpesa_access_token['access_token']
 
     return HttpResponse(validated_mpesa_access_token)
+
+
+def lipa_na_mpesa_online(request):
+    access_token = getAccessToken()
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = {"Authorization": "Bearer %s" % access_token}
+    request = {
+        "BusinessShortCode": "174379",
+        "Password": "",
+        "Timestamp": "",
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": " ",
+        "PartyA": " ",
+        "PartyB": " ",
+        "PhoneNumber": " ",
+        "CallBackURL": "https://ip_address:port/callback",
+        "AccountReference": " ",
+        "TransactionDesc": " "
+    }
+
+    response = requests.post(api_url, json=request, headers=headers)
+    return HttpResponse('success')
+
+
+
